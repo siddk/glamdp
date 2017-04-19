24e161a1543934d347ddf9a37c1be590931d6b5b
Edward Williams <edward_c_williams@brown.edu>
2017-04-19 17:09:31 -0400

    added cleanupdomain config 2

---
+++ data/cd_config_2/ends/cd2_L2.ml
@@ -0,0 +1,352 @@
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+agentInRegion roomIsBlue
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsGreen blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
+agentInRegion roomIsBlue blockInRegion roomIsRed
