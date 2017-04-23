48d709a2fa3e77f15c8a61e6544375129ccd1216
Edward Williams <edward_c_williams@brown.edu>
2017-04-23 18:05:58 -0400

    more segments

--- data/rss_data/means/L0_segmented.ml
+++ data/rss_data/means/L0_segmented.ml
@@ -135,3 +135,70 @@ Down 1 | Left 1 | Up 1 | Right 1 | Down 1 | West 1 | Up 1 | Down 1 | Left 1 | Up
 Down 1 | Left 1 | Up 1 | Right 1 | Down 1 | West 1 | Up 1 | Down 1 | Left 1 | Up 1
 Down 1 | Left 1 | Up 1 | Right 1 | Up 1 | Down 1 | Left 1 | Up 1
 Down 5 | Right 5 | Up 1 | Right 4 | Down 1 | Left 1 | Up 3 | Down 1 | Right 3 | Up 2
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Left 1 | Up 3 | Down 1 | Right 4 | Up 2
+Down 1 | Left 1 | Up 1 | Right 1 | Down 1 | Up 1 | Down 1 | Left 1 | Up 1
+Down 5 | Right 5 | Up 1 | Right 4 | Down 1 | Left 1 | Up 3 | Down 1 | Right 4 | Up 2
+Down 5 | Left 1 | Right 1 | Up 1 | Left 1 | Up 1
+Down 1 | Down 1 | Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Right 1 | Right 1 | Right 1 | Right 1 | Down 1 | Right 1 | Up 1 | Up 1 | Up 1 | Down 1 | Left 1 | Left 1 | Left 1 | Up 1
+Down 1 | Down 1 | Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Right 1 | Right 1 | Right 1 | Right 1 | Down 1 | Right 1 | Up 1 | Up 1 | Up 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Left 1 | Left 1
+Down 1 | Left 1 | Right 1 | Up 1 | Down 1 | Left 1 | Up 1
+Down 1 | Left 1 | Up 1 | Right 1 | Down 1 | Right 1 | Up 1 | Down 1 | Left 1 | Up 1
+Down 5 | Left 5 | Up 1| Right 4 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 5 | Left 5 | Up 1 | Right 1 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 1 | Left 1 | Up 1 | Right 1 | Down 1 | Right 1 | Up 1 | Down 1 | Left 1 | Up 1
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Down 2 | Left 4 | Up 2
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 1 | Right 1 | Up 1 | Right 1 | Up 1 | Down 1 | Right 1 | Up 1
+Down 1 | Left 1 | Up 1 | Right 1 | Down 1 | Up 1 | Down 1 | Left 1 | Up 1
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 1 | Left 1 | Right 1 | Up 1 | Down 1 | Left 1 | Up 1
+Down 1 | Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Right 1 | Right 1 | Right 1 | Down 1 | Right 1 | Up 1 | Up 1 | Up 1 | Down 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 1 | Right 1 | Up 1 | Right 1 | Down 1 | Right 1 | Up 1 | Down 1 | Right 1 | Up 1
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 1 | Left 1 | Up 1 | Right 1 | Down 1 | Right 1 | Up 1 | Down 1 | Left 1 | Up 1
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Left 4 | Up 2
+Down 1 | Left 1 | Up 1 | Right 1 | Up 1 | Down 1 | Right 1 | Up 1
+Down 1 | Down 1 | Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Right 1 | Right 1 | Right 1 | Right 1 | Down 1 | Right 1 | Up 1 | Up 1 | Up 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 1 | Right 1 | Up 1 | Left 1 | Down 1 | Up 1 | Down 1 | Right 1 | Up 1
+Down 1 | Right 1 | Up 1 | Right 1 | Up 1 | Down 1 | Right 1 | Up 1
+Down 5 | Right 5 | Up 1 | Right 4 | Down 1 | Left 1 | Up 3 | Down 1 | Right 3 | Up 1
+Down 1 | Down 1 | Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Right 1 | Right 1 | Right 1 | Right 1 | Down 1 | Right 1 | Up 1 | Up 1 | Up 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 1 | Left 1 | Up 1 | Right 1 | Down 1 | Up 1 | Left 1 | Up 1
+Down 5 | Left 5 | Up 1 | Right 4 | Down 1 | Right 1 | Up 3 | Down 1 | Left 4 | Up 2
+Down 1 | Left 1 | Up 1
+Down 3 | Left 4 | Up 2
+Down 1 | Right 1 | Up 1
+Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 1 | Left 1 | Up 1
+Down 3 | Left 4 | Up 2
+Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1 | Up 1
+Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 3 | Left 4 | Up 3
+Down 3 | Left 4 | Up 3
+Down 1 | Left 1 | Up 1
+Down 3 | Left 4 | Up 2
+Down 3 | Left 2 | Up 2
+Down 3 | Left 4 | Up 2
+Down 1 | Left 1
+Down 1 | Left 1 | Up 1
+Down 1 | Left 1 | Up 1
+Down 3 | Left 4 | Up 2
+Down 3 | Left 4 | Up 2
+Down 3 | Left 4 | Up 2
+Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 3 | Left 4 | Up 2
+Down 3 | Left 4 | Up 2
+Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 1 | Left 1 | Up 1
+Down 2 | Left 3 | Up 2
+Down 1 | Left 1 | Up 1
+Down 3 | Left 4 | Up 2
+Down 1 | Left 1 | Up 1
+Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 3 | Left 4 | Up 2
+Down 1 | Down 1 | Down 1 | Left 1 | Left 1 | Left 1 | Left 1 | Up 1 | Up 1
+Down 1 | Left 1 | Up 1
